b9953a0452b75df007f67f5d9844856551c5f338
Gabriel Camargo <gabriel.rodrigocam@gmail.com>
2019-05-12 11:30:11 -0300

    2.1 - Capturando dados do formulário

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,5 +1,4 @@
-const negociacao = new Negociacao(new Date(), 1, 100);
-//negociacao._quantidade = 3; // O VSCODE indica um erro de compilação aqui
-console.log(negociacao.quantidade);
-
-alert("opa");
+const controller = new NegociacaoController();
+document
+    .querySelector('.form')
+    .addEventListener('submit',controller.adiciona.bind(controller));
